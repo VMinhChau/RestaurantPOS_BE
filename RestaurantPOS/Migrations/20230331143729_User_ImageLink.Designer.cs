5c638d3f747cc15f41cf93366660a8e057e674f0
TUANNKA\anhtu <tuan.nguyen991@hcmut.edu.vn>
2023-03-31 21:38:36 +0700

    Add ImageLink field in User

--- RestaurantPOS/Migrations/20230331143729_User_ImageLink.Designer.cs
+++ RestaurantPOS/Migrations/20230331143729_User_ImageLink.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantPOS.Data;
 
@@ -11,9 +12,11 @@ using RestaurantPOS.Data;
 namespace RestaurantPOS.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class RestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230331143729_User_ImageLink")]
+    partial class User_ImageLink
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
